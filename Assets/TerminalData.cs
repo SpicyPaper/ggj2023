0bc6f35b7e5e834c3e39031d201eb29fe1347ebd
Alexander Wohlfahrt <akawohlfahrt@gmail.com>
2023-02-04 00:23:47 +0100

    Add Terminal data script and create prefab for Terminal

---
+++ Assets/TerminalData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TerminalData : MonoBehaviour
+{
+    public TMP_Text TerminalTitle;
+    public List<TMP_Text> RamSteps;
+    public TMP_Text UserAndComputer;
+    public TMP_Text Path;
+    public TMP_Text Command;
+}
